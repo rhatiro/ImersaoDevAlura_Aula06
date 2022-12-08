f4815da0a5845b28f8d4ae7de08bc89efc70f2f8
rhatiro <roberto.nishiyama@yahoo.com.br>
2022-12-08 20:43:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Imersão Dev Alura - Aula 06 - Objetos e tabelas de classificação
+----------------------------------------------------------------
+https://imersao.dev/aulas/aula06-objetos-tabelas-classificacao
+
+Conteúdo detalhado desta aula:
+
+- Remover o código estático do HTML;
+- Criar um objeto no Javascript para cada jogador;
+- Criar uma função que receba um objeto como parâmetro para calcular os pontos;
+- Exibir o objeto na página HTML;
+- Criar uma função para adicionar vitória, outra para adicionar empate e uma última para adicionar derrota;
+- Recalcular os pontos quando vitória ou empate for adicionado.
+
+Desafios desta aula!
+
+- Fazer a lógica de quando houver um empate, obrigatóriamente deveria já ajustar como empate para os demais jogadores;
+- Validar se todos os pontos estão fazendo sentido, tanto o número de empates, quanto derrotas e vitórias com os demais jogadores (impossível haver mais vitórias que derrotas, por exemplo);
+- Adicionar a imagem de cada jogador;
+- Criar um botão para zerar todos os pontos;
+- Criar um botão e inputs (campos de texto) para adicionar novos jogadores, com seus respectivos dados;
+- Utilizar seu jogo preferido para se basear na pontuação da sua tabela de classificação.
+
+A [Pen](https://codepen.io/rhatiro/pen/ExRLqWR) by [rhatiro](https://codepen.io/rhatiro) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/ExRLqWR).
---
+++ script.js
@@ -0,0 +1,111 @@
+var papaiRob = {
+  nome: "Papai Rob",
+  emoji: "🧙‍♂️",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var ravi = {
+  nome: "Ravi",
+  emoji: "🚴‍",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+var michi = {
+  nome: "Michi",
+  emoji: "🐈",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+
+papaiRob.pontos = calculaPontos(papaiRob);
+ravi.pontos = calculaPontos(ravi);
+michi.pontos = calculaPontos(michi);
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+var jogadores = [papaiRob, ravi, michi];
+
+function exibeJogadoresNaTela(jogadores) {
+  var elemento = "";
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + jogadores[i].emoji + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  for (var index = 0; index < jogadores.length; index++) {
+    if (index == i) {
+      continue;
+    }
+    jogadores[index].derrotas++;
+  }
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate() {
+  for (var index = 0; index < jogadores.length; index++) {
+    jogadores[index].empates++;
+    jogadores[index].pontos = calculaPontos(jogadores[index]);
+  }
+
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibeJogadoresNaTela(jogadores);
+}
+
+function zerarPontos() {
+  for (var index = 0; index < jogadores.length; index++) {
+    jogadores[index].vitorias = 0;
+    jogadores[index].empates = 0;
+    jogadores[index].derrotas = 0;
+    jogadores[index].pontos = calculaPontos(jogadores[index]);
+  }
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionar() {
+  var nome = document.getElementById("novoNome").value;
+  jogadores.push({
+    nome: nome,
+    emoji: "",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+  });
+  exibeJogadoresNaTela(jogadores);
+  document.getElementById("novoNome").value = "";
+  // console.log(jogadores);
+  event.preventDefault();
+}
